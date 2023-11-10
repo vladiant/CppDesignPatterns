61e07a04bdf33d3a62f5c16267bb31ab20031742
Vladislav Antonov <vladislav.antonov@gmail.com>
2023-11-10 08:33:22 +0200

    Add Interpeter example

---
+++ Behavioral/Interpreter/interpreter.cpp
@@ -0,0 +1,71 @@
+// https://www.youtube.com/watch?v=MEejmuLwX9M
+// Design Patterns: Examples in C++ - Chris Ryan - ACCU 2023
+// Interpreter (Behavioral)
+// https://godbolt.org/z/ad84qT79x
+
+#include <iostream>
+#include <memory>
+#include <string>
+#include <map>
+
+using Context = std::map<std::string, int>;
+using PtrExp = std::shared_ptr<class Expression>;
+
+// Abstract Expression
+struct Expression {
+    virtual int interpret(Context& ctx) = 0;
+};
+
+struct Variable : Expression {
+    Variable(std::string name) : name(name) {}
+    int interpret(Context& ctx) {
+        return ctx[name];
+    }
+    std::string name;
+};
+
+struct Constant : Expression {
+    Constant(int value) : value(value) {}
+    int interpret(Context& ctx) {
+        return value;
+    }
+    int value;
+};
+
+struct Add : Expression {
+    Add(PtrExp left, PtrExp right) : left(left), right(right) {}
+    int interpret(Context& ctx) {
+        return left->interpret(ctx) + right->interpret(ctx);
+    }
+    PtrExp left, right;
+};
+
+struct Subtract : Expression {
+    Subtract(PtrExp left, PtrExp right) : left(left), right(right) {}
+    int interpret(Context& ctx) {
+        return left->interpret(ctx) - right->interpret(ctx);
+    }
+    PtrExp left, right;
+};
+
+// Client
+int main() {
+
+    // Create the expression tree
+    // result = ((x+2)-y)
+    PtrExp expr = 
+        std::make_shared<Subtract>(
+            std::make_shared<Add>(
+                std::make_shared<Variable>("x"),
+                std::make_shared<Constant>(2)),
+            std::make_shared<Variable>("y")
+        );
+
+    Context context;
+    context["x"] = 10;
+    context["y"] = 5;
+
+    // Interpret the expression
+    std::cout << "Result: " << expr->interpret(context) << "\n";
+}
+
